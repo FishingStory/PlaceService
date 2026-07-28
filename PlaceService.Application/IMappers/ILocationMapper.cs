bea48e8c3c85c8215df9c64579e08fb5fb7721ba
Denys Prokhorenko <prokhorenkodenys456@gmail.com>
2026-07-28 14:25:25 +0200

    feat: implemented object-to-object mapping for location model fix: fixed the signature of the methods in ILocationService (#2)

---
+++ PlaceService.Application/IMappers/ILocationMapper.cs
@@ -0,0 +1,14 @@
+using PlaceService.Domain.Entities.Models;
+using PlaceService.Application.DTOs;
+
+namespace PlaceService.Application.IMappers;
+
+public interface ILocationMapper
+{
+    public Location MapCreateLocationDtoToLocation(CreateLocationDto location);
+    
+    public Location MapUpdateLocationDtoToLocation(UpdateLocationDto location);
+    
+    public ResponseLocationDto MapLocationToResponseLocationDto(Location location);
+    
+}
